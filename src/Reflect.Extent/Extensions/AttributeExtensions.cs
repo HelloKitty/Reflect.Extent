f5847b9d0bb5c4dd720999815708dd62c147b45c
HelloKitty <andrew.blakely@ymail.com>
2017-02-27 22:10:31 -0600

    Added MemberInfo HasAttribute extension

---
+++ src/Reflect.Extent/Extensions/AttributeExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Reflect.Extent
+{
+	/// <summary>
+	/// Extension methods for attribute reading and parsing.
+	/// </summary>
+    public static class AttributeExtensions
+    {
+		/// <summary>
+		/// Checks if the provided <see cref="MemberInfo"/> <paramref name="provider"/> contains
+		/// the specified <typeparamref name="TAttributeType"/>.
+		/// </summary>
+		/// <typeparam name="TAttributeType">The attribute type to check for.</typeparam>
+		/// <param name="provider">The <see cref="MemberInfo"/> provider.</param>
+		/// <returns>True if the <see cref="MemberInfo"/> is marked with the <typeparamref name="TAttributeType"/> attribute.</returns>
+	    public static bool HasAttribute<TAttributeType>(this MemberInfo provider)
+			where TAttributeType : Attribute
+
+	    {
+		    return provider.GetCustomAttribute<TAttributeType>() != null;
+	    }
+
+		//TODO: Maybe use?
+	    private static bool CheckSubclassedAttribute(Type requested, Type acual)
+	    {
+		    if (requested == null) throw new ArgumentNullException(nameof(requested));
+		    if (acual == null) throw new ArgumentNullException(nameof(acual));
+
+		    return true;
+	    }
+    }
+}
---
+++ tests/Reflect.Extent.Tests/AttributeExtensionsTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Reflect.Extent.Tests
+{
+	[TestFixture]
+	public class AttributeExtensionsTests
+	{
+		[Test]
+		public void Test_HasAttribute_Sees_Same_Type_Attribute()
+		{
+			//arrange
+			MemberInfo member = typeof(TestAttributeClass).GetMember("TestField").First();
+
+			//act
+			bool result = member.HasAttribute<TestAttribute1>();
+
+			//assert
+			Assert.IsTrue(result);
+		}
+
+		[Test]
+		public void Test_HasAttribute_Doesnt_See_Unmarked_Attribute()
+		{
+			//arrange
+			MemberInfo member = typeof(TestAttributeClass).GetMember("TestField").First();
+
+			//act
+			bool result = member.HasAttribute<ApartmentAttribute>();
+
+			//assert
+			Assert.IsFalse(result);
+		}
+
+		[Test]
+		public void Test_HasAttribute_Can_See_Derived_From_Base()
+		{
+			//arrange
+			MemberInfo member = typeof(TestAttributeClass).GetMember("TestProp").First();
+
+			//act
+			bool result = member.HasAttribute<TestAttribute1>();
+
+			//assert
+			Assert.True(result);
+		}
+
+		public class TestAttribute1 : Attribute
+		{
+			
+		}
+
+		public class TestAttribute2 : TestAttribute1
+		{
+			
+		}
+
+		[TestAttribute1]
+		public class TestAttributeClass
+		{
+			[TestAttribute1]
+			public int TestField;
+
+			[TestAttribute2]
+			public double TestProp { get; }
+		}
+	}
+}
